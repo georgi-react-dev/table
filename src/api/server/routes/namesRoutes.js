ae3702a4e8d1f4405551aaf97c58a64a7d838d96
Georgi Ivanov <georgi.react.dev@gmail.com>
2022-06-28 22:00:22 +0300

    separate front and backend

--- src/api/server/routes/namesRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getNames } = require("../controllers/namesController");
-
-router.get("/", getNames);
-
-module.exports = router;
--- src/api/server/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUsers, getUser, saveUser } = require("../controllers/usersController");
-
-router.get("/", getUsers);
-router.get("/:id", getUser);
-router.post("/save", saveUser);
-
-module.exports = router;
